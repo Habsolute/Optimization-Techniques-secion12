f8508ef61f71ed63b6e80b06cf986e9334a129e2
Habsolute <85709216+Habsolute@users.noreply.github.com>
2022-06-03 16:27:17 -0400

    final push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 
@@ -6,12 +6,19 @@ import "./App.css";
 
 function App() {
   const [showP, setShowP] = useState(false);
+  const [allowToggle, setallowToggle] = useState(false);
 
   console.log("appRuning");
 
-  const toggleParagraphHandler = () => {
-    // console.log("click button");
-    setShowP((prevShowpP) => !prevShowpP);
+  const toggleParagraphHandler = useCallback(() => {
+    console.log("click button");
+    if (allowToggle) {
+      setShowP((prevShowpP) => !prevShowpP);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setallowToggle(true);
   };
 
   // function toggleParagraphHandler() {
@@ -22,6 +29,7 @@ function App() {
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showP} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
